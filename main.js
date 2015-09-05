2881e45e9fc2d28826bcf37d5ca43cc1745ecd98
saan1984 <sandeep_giet@yahoo.com>
2015-09-05 23:33:36 +0530

    changes

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import $ from 'github:components/jquery@2.1.4';
 import bootstrap from 'twbs/bootstrap';
+import popover from 'twbs/bootstrap/js/umd/popover';
 import 'twbs/bootstrap/css/bootstrap.css!';
+
